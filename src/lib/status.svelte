469f9944f3cd83b6cc76231b68a90cbf246a26d3
Rafi Strauss <rafikis75@gmail.com>
2021-10-31 00:10:10 -0400

    Initial commit

---
+++ src/lib/status.svelte
@@ -0,0 +1,23 @@
+<script>
+	/** @type {boolean} Eruv Status; TRUE is up, FALSE is down */
+	export let status;
+</script>
+
+{#if status}
+	<!-- content here -->
+	<span class="eruvUp"> UP </span>
+{:else}
+	<!-- else content here -->
+	<span class="eruvDown"> DOWN </span>
+{/if}
+
+<style>
+	/* your styles go here */
+	.eruvUp {
+		color: #508d24;
+	}
+
+	.eruvDown {
+		color: rgb(141, 36, 36);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    // your script goes here
+    import Status from '$lib/status.svelte';
+    export let status, date;
+</script>
+
+<h1>
+    The Eruv is <Status {status} /> as of {date}.
+</h1>
+
+<style>
+    h1 {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+</style>
