29b93347e210252270b9e27fd8454f3198272987
Rafi Strauss <rafikis75@gmail.com>
2021-12-26 01:40:23 -0500

    Add route so minyanim endpoint is built

---
+++ src/routes/asdf.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	export const router = false;
+	export const hydrate = false;
+
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ page, fetch, session, stuff }) {
+		const url = `/minyanim.json`;
+		const res = await fetch(url);
+
+		if (res.ok) {
+			const data = await res.json();
+			return {
+				body: {
+					minyanim: data,
+					status: 200
+				}
+			};
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Shenk Minyanim</title>
+</svelte:head>
