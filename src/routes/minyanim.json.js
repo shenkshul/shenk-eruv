2ed4163de79fdcc15dd06f78970c8a6d9392fd9b
Rafi Strauss <rafikis75@gmail.com>
2022-06-02 08:59:31 -0400

    Fix minyan imports to avoid the extra default key

--- src/routes/minyanim.json.js
+++ src/routes/minyanim.json.js
@@ -1,14 +1,14 @@
-import * as Sunday from '$lib/minyanim/Sunday.json';
-import * as Monday from '$lib/minyanim/Monday.json';
-import * as Tuesday from '$lib/minyanim/Tuesday.json';
-import * as Wednesday from '$lib/minyanim/Wednesday.json';
-import * as Thursday from '$lib/minyanim/Thursday.json';
-import * as Friday from '$lib/minyanim/Friday.json';
-import * as Shabbos from '$lib/minyanim/Shabbos.json';
-import * as Shavuos from '$lib/minyanim/Shavuos.json';
-import * as Shavuos_2 from '$lib/minyanim/Shavuos_2.json';
-import * as Erev_Purim from '$lib/minyanim/Erev_Purim.json';
-import * as Purim from '$lib/minyanim/Purim.json';
+import Sunday from '$lib/minyanim/Sunday.json';
+import Monday from '$lib/minyanim/Monday.json';
+import Tuesday from '$lib/minyanim/Tuesday.json';
+import Wednesday from '$lib/minyanim/Wednesday.json';
+import Thursday from '$lib/minyanim/Thursday.json';
+import Friday from '$lib/minyanim/Friday.json';
+import Shabbos from '$lib/minyanim/Shabbos.json';
+import Shavuos from '$lib/minyanim/Shavuos.json';
+import Shavuos_2 from '$lib/minyanim/Shavuos_2.json';
+import Erev_Purim from '$lib/minyanim/Erev_Purim.json';
+import Purim from '$lib/minyanim/Purim.json';
 
 const minyanim = {
 		Sunday,
